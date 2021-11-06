c306b2942e846fdd296d4cf674db41d80abad0ba
ManyaSharma101007 <manyashettysharma@gmail.com>
2021-11-06 10:47:04 +0530

    S

--- App.js
+++ App.js
@@ -21,20 +21,12 @@ const TabNavigator = createBottomTabNavigator({
   defaultNavigationOptions: ({navigation})=>({
     tabBarIcon: ()=>{
       
-      routeName
-      const routeName = navigation.routeName;
       const routeName = navigation.state.routeName;
-      const routeName = navigation.state;
-      const routeName = state.routeName.navigation;
-       
     
       if(routeName === "WriteStory"){
         return(
           <Image
-           source={require("assets/write.png")}
-           source={require("./write.png")}
            source={require("./assets/write.png")}
-           source={require("write.png")}
           style={{width:40, height:40}}
         />
         )
@@ -43,9 +35,6 @@ const TabNavigator = createBottomTabNavigator({
       else if(routeName === "ReadStory"){
         return(
           <Image
-           source={require("./read.png")}
-           source={require("read.png")}
-           source={require("./assets")}
            source={require("./assets/read.png")}
           style={{width:40, height:40}}
         />)
